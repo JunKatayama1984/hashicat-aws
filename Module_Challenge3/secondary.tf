f20ff3373ee403de823faf02d7ababeaa725f023
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 17:38:35 +1100

    Update secondary.tf

--- Module_Challenge3/secondary.tf
+++ Module_Challenge3/secondary.tf
@@ -1,5 +1,6 @@
 module "secondary_network" {
-  source = "./modules/network"
+  source  = "app.terraform.io/example-org-a3f09c/network/aws"
+  version = "0.0.1"
   CIDR   = var.secondary_vpc
   region = var.secondary_region
   networks = var.secondary_network
