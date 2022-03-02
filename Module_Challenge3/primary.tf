4590739beb7594c985e3ac45708dca6ec9811cca
JunKatayama1984 <46908046+JunKatayama1984@users.noreply.github.com>
2022-03-02 17:38:12 +1100

    Update primary.tf

--- Module_Challenge3/primary.tf
+++ Module_Challenge3/primary.tf
@@ -1,5 +1,6 @@
 module "primary_network" {
-  source = "./modules/network"
+  source  = "app.terraform.io/example-org-a3f09c/network/aws"
+  version = "0.0.1"
   CIDR   = var.primary_vpc
   region = var.primary_region
   networks = var.primary_network
